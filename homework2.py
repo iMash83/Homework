57ac65a63251b2c5b6ec398567f237bd3f8c9237
iMash83 <mash2biz@gmail.com>
2026-02-12 13:00:04 +0100

    changes

--- homework2.py
+++ homework2.py
@@ -1,14 +1,19 @@
 import random
 
-def get_numbers_ticket(min_val, max_val, quantity):
- 
-    if not (1 <= min_val <= quantity <= max_val <= 1000):
+def get_numbers_ticket(min, max, quantity):
+    # Перевірка відповідності параметрів вимогам
+    if not (1 <= min <= quantity <= max <= 1000):
         return []
 
-    numbers_range = range(min_val, max_val + 1)
-    lucky_numbers = random.sample(numbers_range, quantity)
-    
-    return sorted(lucky_numbers)
+    # Створення набору випадкових чисел у заданому діапазоні
+    try:
+        numbers = random.sample(range(min, max + 1), quantity)
+    except ValueError:
+        return []
 
+    # Повернення відсортованого списку
+    return sorted(numbers)
+
+# Приклад 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
